3a36fff1a4f731e5c1845f1cd135eac4340e15b3
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-01-12 15:11:06 -0500

    src/screens/SignUpScreen.js

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
 const SignUpScreen = ({ navigation }) => {
     return (
-        <>
-            <Text>SignUpScreen</Text>
-            <Button 
-                title="Go to Signin" 
-                onPress={() => navigation.navigate('SignIn')}
-            />
-            <Button 
-                title="Go to main flow" 
-                onPress={() => navigation.navigate('mainFlow')}
-            />
-        </>
+        <View style={styles.container}>
+            <Spacer>
+                <Text h3>Sign Up for Tracker</Text>
+            </Spacer>
+            <Input label="Email" />
+            <Spacer />
+            <Input label="Password" />
+            <Spacer>
+                <Button title="Sign Up"/>
+            </Spacer>
+            
+        </View>
     )
 }
 
-const styles = StyleSheet.create({
+// removes the header from the screen
+SignUpScreen.navigationOptions = () => {
+    return {
+      headerShown: false,
+    };
+  };
 
+const styles = StyleSheet.create({
+    container: {
+        // borderColor: 'red',
+        // borderWidth: 10,
+        flex: 1 ,
+        justifyContent: 'center',
+        marginBottom: 200
+    }
 });
 
 export default SignUpScreen;
+
