df2d4f371061a78f49a0ea4824628bd12ea71289
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-01-13 09:32:10 -0500

    Update signup action function

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,3 +1,4 @@
+import { AsyncStorage } from '@react-native-async-storage/async-storage';
 import createDataContext from './createDataContext';
 import trackerApi from '../api/tracker';
 
@@ -5,24 +6,27 @@ const authReducer = (state, action) => {
     switch(action.type) {
         case 'add_error':
             return { ...state, errorMessage: action.payload }
+            case 'signup':
+                // we don't need existing state '...state'
+                // reset errorMessage
+                return {errorMessage: '', token: action.payload} 
         default:
             return state;
     }
 };
 
 /**ACTION FUNCTIONS */
-const signup = (dispatch) => {
-    return async ({ email, password }) => {
+const signup = (dispatch) => async ({ email, password }) => {
+    try {
         // make api request to sign up with email and password
+        const response = await trackerApi.post('/signup', { email, password });
         // if we sign up, modify our state, and say that we are authenticated
+        await AsyncStorage.setItem('token', response.data.token);
+        dispatch({ type: 'signup', payload: response.data.token })
+    } catch (err) {
         // if signing up fails, we probably need to reflect an error message
-        try {
-            const response = await trackerApi.post('/signup', { email, password });
-            console.log(response.data)
-        } catch (err) {
-            dispatch({ type: 'add_error', payload: 'Something went wrong with sign up' })
-        }
-    };
+        dispatch({ type: 'add_error', payload: 'Something went wrong with sign up' })
+    }
 };
 
 const signin = (dispatch) => {
@@ -49,5 +53,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
     authReducer,
     { signup, signin, signout }, // action functions
-    { isSignedIn: false, errorMessage: '' } // initial state object
+    { token: null, errorMessage: '' } // initial state object // no token? not logged in
 );
