f2febe6c1c84c5b27f6522e8bbb922ddd20070af
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-01-13 08:56:18 -0500

    Update ngrok link

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// after 8 hours take the new URL from ngrok and replace baseURL
+// after  hours take the new URL from ngrok and replace baseURL
 export default axios.create({
-    baseURL: 'http://73a0ea70fcd9.ngrok.io'
+    baseURL: 'http://01862d287091.ngrok.io'
 });
