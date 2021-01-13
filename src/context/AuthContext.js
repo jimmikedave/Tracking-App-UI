cc537f6f2e11917a74c2c69cc319884ace8479c8
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-01-12 19:26:45 -0500

    Add action function templates

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,8 +7,40 @@ const authReducer = (state, action) => {
     }
 };
 
+/**ACTION FUNCTIONS */
+const signup = (dispatch) => {
+    return ({ email, password }) => {
+        // make api request to sign up with email and password
+
+        // if we sign up, modify our state, and say that we are authenticated
+
+        // if signing up fails, we probably need to reflect an error message
+    };
+};
+
+const signin = (dispatch) => {
+    return ({ email, password }) => {
+        // try to sign in
+
+        // handle succes by updating state
+
+        // handle failure by showing error message
+    };
+};
+
+const signout = (dispatch) => {
+    return ({ email, password }) => {
+        // sign user out
+
+        // handle success by updating state
+
+        // handle failure by showing error message
+
+    }
+};
+
 export const { Provider, Context } = createDataContext(
     authReducer,
-    {},
+    { signup, signin, signout },
     { isSignedIn: false }
 );
