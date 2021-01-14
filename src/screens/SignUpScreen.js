112ff56e8c231029c1410653bb6a0b7f7a467303
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-01-14 17:47:07 -0500

    Configure Signup screen and navigation

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,9 +1,8 @@
 import React, { useContext } from 'react';
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
-import { Text } from 'react-native-elements';
-import Spacer from '../components/Spacer';
 import { Context as AuthContext } from '../context/AuthContext';
 import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 
 const SignUpScreen = ({ navigation }) => {
     const { state, signup } = useContext(AuthContext);
@@ -16,11 +15,10 @@ const SignUpScreen = ({ navigation }) => {
                 onSubmit={signup}
                 submitText="Sign Up"
             />
-            <TouchableOpacity onPress={() => navigation.navigate('SignIn')}>
-                <Spacer>
-                    <Text style={styles.link}>Already have an account? Sign in instead.</Text>
-                </Spacer>
-            </TouchableOpacity>
+            <NavLink 
+                routeName="SignIn"
+                text="Already have an account? Sign in instead."
+            />
         </View>
     )
 }
@@ -39,9 +37,6 @@ const styles = StyleSheet.create({
         flex: 1 ,
         justifyContent: 'center',
         marginBottom: 200
-    },
-    link: {
-        color: 'blue'
     }
 });
 
