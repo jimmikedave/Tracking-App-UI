b1e76ca5466c28920ee6e4f8305fb752ed5b0ca5
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-01-15 12:12:19 -0500

    Add clearErrorMessage function to NavigationEvents

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,14 +1,18 @@
 import React, { useContext } from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import { NavigationEvents } from 'react-navigation';
 import AuthForm from '../components/AuthForm';
 import NavLink from '../components/NavLink';
 import { Context } from '../context/AuthContext';
 
 const SignInScreen = () => {
-    const { state, signin } = useContext(Context);
+    const { state, signin, clearErrorMessage } = useContext(Context);
 
     return (
         <View style={styles.container}>
+        <NavigationEvents 
+            onWillFocus={clearErrorMessage}
+        />
             <AuthForm
                 headerText="Sign In To Your Account"
                 errorMessage={state.errorMessage}
