1aff6a2dd2c18549a1a8316b2ed9670fe8dcf5a4
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-01-15 12:57:15 -0500

    Remove token check function

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,17 +1,12 @@
-import React, { useContext, useEffect } from 'react';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useContext } from 'react';
+import { View, StyleSheet } from 'react-native';
 import { NavigationEvents } from 'react-navigation';
 import { Context as AuthContext } from '../context/AuthContext';
 import AuthForm from '../components/AuthForm';
 import NavLink from '../components/NavLink';
 
 const SignUpScreen = () => {
-    const { state, signup, clearErrorMessage, tryLocalSignin } = useContext(AuthContext);
-
-    // automatically signs in if a token exists
-    useEffect(() => {
-        tryLocalSignin();
-    }, [])
+    const { state, signup, clearErrorMessage} = useContext(AuthContext);
 
     return (
         <View style={styles.container}>
