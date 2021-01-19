889b46a25048de820fb78f7b290cefb57e083eeb
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-01-19 11:17:48 -0500

    Stop tracking if user is not on page

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -1,28 +1,37 @@
 import { useState, useEffect } from 'react';
 import { Accuracy, requestPermissionsAsync, watchPositionAsync } from 'expo-location';
 
-export default (callback) => {
+export default (shouldTrack, callback) => {
     const [ err, setErr ] = useState(null);
+    const [subscriber, setSubscriber] = useState(null);
 
     const startWatching = async () => {
         try {
             await requestPermissionsAsync();
-            await watchPositionAsync({
+            const sub = await watchPositionAsync({
                 accuracy: Accuracy.BestForNavigation,
                 timeInterval: 1000, //update every second
                 distance: 10, //update every 10 meters
             }, 
             callback
             );
-                
+            setSubscriber(sub);
         } catch (e) {
             setErr(e)
         }
     };
 
+    // putting should track in the array or a value in [] tells react to run
+    // each time [shouldTrack] changes
     useEffect(() => {
-        startWatching();
-    }, []);
+        if(shouldTrack) {
+            startWatching();
+        } else {
+            //stop watching
+            subscriber.remove();
+            setSubscriber(null);
+        }
+    }, [shouldTrack]); 
 
     return [err]
 };
