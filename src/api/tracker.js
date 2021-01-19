fb37094f27ece8ad85eb6324b904b92f27f6659f
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-01-19 16:58:10 -0500

    Automate authentication with jwt

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,6 +1,23 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // after  hours take the new URL from ngrok and replace baseURL
-export default axios.create({
-    baseURL: 'http://84f8481729f9.ngrok.io'
+const instance = axios.create({
+    baseURL: 'http://67eb03915106.ngrok.io'
 });
+
+// automatically adds token to each request
+instance.interceptors.request.use(
+    async (config) => {
+        const token = await AsyncStorage.getItem('token');
+        if(token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config
+    },
+    (err) => {
+        return Promise.reject(err);
+    }
+);
+
+export default instance;
