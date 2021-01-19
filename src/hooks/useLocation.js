4c6566e2cb19f3e88e3fc945cb4e5798e28a7489
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-01-19 16:01:14 -0500

    Prevent Stale references

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -3,34 +3,42 @@ import { Accuracy, requestPermissionsAsync, watchPositionAsync } from 'expo-loca
 
 export default (shouldTrack, callback) => {
     const [ err, setErr ] = useState(null);
-    const [subscriber, setSubscriber] = useState(null);
-
-    const startWatching = async () => {
-        try {
-            await requestPermissionsAsync();
-            const sub = await watchPositionAsync({
-                accuracy: Accuracy.BestForNavigation,
-                timeInterval: 1000, //update every second
-                distance: 10, //update every 10 meters
-            }, 
-            callback
-            );
-            setSubscriber(sub);
-        } catch (e) {
-            setErr(e)
-        }
-    };
 
     // putting should track in the array or a value in [] tells react to run
     // each time [shouldTrack] changes
     useEffect(() => {
+        let subscriber;
+        const startWatching = async () => {
+            try {
+                await requestPermissionsAsync();
+                subscriber = await watchPositionAsync({
+                    accuracy: Accuracy.BestForNavigation,
+                    timeInterval: 1000, //update every second
+                    distance: 10, //update every 10 meters
+                }, 
+                    callback
+                );
+                setSubscriber(sub);
+            } catch (e) {
+                setErr(e)
+            }
+        };
+
         if(shouldTrack) {
             startWatching();
         } else {
-            //stop watching
-            subscriber.remove();
-            setSubscriber(null);
+            if(subscriber) {
+                //stop watching
+                subscriber.remove();
+            } 
+            subscriber = null;
         }
+
+        return () => {
+            if(subscriber) {
+                subscriber.remove();
+            }
+        };
     }, [shouldTrack, callback]); // new value callback only if state.recording changes
 
     return [err]
