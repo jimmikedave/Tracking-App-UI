16b1d42eb4d5375ded0e693d693d4abdd294dc0f
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-01-19 16:57:49 -0500

    Add track context

---
+++ src/context/TrackContext.js
@@ -0,0 +1,21 @@
+import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
+
+const trackReducer = (state, action) => {
+    switch (action.type) {
+        default: 
+            return state;
+    }
+};
+
+const fetchTracks = dispatch => () => {};
+const createTrack = dispatch => (name, locations) => {
+    //make a request to our api
+    console.log(name, locations.length)
+};
+
+export const { Provider, Context } = createDataContext(
+    trackReducer,
+    { fetchTracks, createTrack },
+    []
+); 
