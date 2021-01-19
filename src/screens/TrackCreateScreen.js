99dfd29ff544e0613f2f2f6f1f87994c24301f32
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-01-19 09:15:28 -0500

    Update mock location

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,19 +1,26 @@
+// import '../_mockLocation';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
-import { requestPermissionsAsync } from 'expo-location';
+import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
 import Map from '../components/Map';
+
 
 const TrackCreateScreen = () => {
     const [ err, setErr ] = useState(null);
 
     const startWatching = async () => {
         try {
-            const { granted } = await requestPermissionsAsync();
-            if(!granted) {
-                throw new Error('Location permission not granted');
-            }
+            await requestPermissionsAsync();
+            await watchPositionAsync({
+                accuracy: Accuracy.BestForNavigation,
+                timeInterval: 1000, //update every second
+                distance: 10, //update every 10 meters
+            }, (location) => {
+                console.log(location)
+            });
+                
         } catch (e) {
             setErr(e)
         }
